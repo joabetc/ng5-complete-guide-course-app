bc5fc11a349f8f6ea03b10f114e33962175cdbac
Joabe Costa <joabetc@hotmail.com>
2018-04-19 18:28:17 -0300

    Adding a button to ServersCompoment template

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,9 +4,10 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
